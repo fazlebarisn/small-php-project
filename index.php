961279c41c08c9d259424a22484b24725be97ca4
Fazle Bari <fazlebarisn@hotmail.com>
2022-11-18 01:21:52 +0600

    working

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+require("functions.php"); 
+$uri = $_SERVER['REQUEST_URI'];
+// require("controllers/index.php"); 
+// require("controllers/about.php"); 
+// require("controllers/contact.php"); 
 
-
-require("controllers/index.php"); 
+ dd($_SERVER['REQUEST_URI']);
+if( $uri == '/small-php-project/'){
+    require("controllers/index.php"); 
+}
