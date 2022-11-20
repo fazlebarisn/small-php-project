6bb0b8c54e41549858c24d1e6fec0b9946948444
Fazle Bari <fazlebarisn@gmail.com>
2022-11-20 12:01:55 +0600

    some thing

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 require("functions.php"); 
-$uri = $_SERVER['REQUEST_URI'];
+// $uri = $_SERVER['REQUEST_URI'];
 // require("controllers/index.php"); 
 // require("controllers/about.php"); 
 // require("controllers/contact.php"); 
 
- dd($_SERVER['REQUEST_URI']);
-if( $uri == '/small-php-project/'){
-    require("controllers/index.php"); 
-}
+//  dd($_SERVER['REQUEST_URI']);
+// if( $uri == '/small-php-project/'){
+//     require("controllers/index.php"); 
+// }
