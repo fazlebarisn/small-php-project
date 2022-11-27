8c3c2183950790d7882924e02588fc6d55bfd7ba
Fazle Bari <fazlebarisn@gmail.com>
2022-11-27 11:02:26 +0600

    database connection

--- index.php
+++ index.php
@@ -3,4 +3,33 @@ require("functions.php");
 
 $heading = 'Home';
 
-require("views/index.view.php"); 
+//require("views/index.view.php"); 
+
+// connect to the database
+class Database{
+
+    public $connection;
+
+    public function __construct()
+    {
+        $dsn = ("mysql:host=localhost; dbname=small_php_project; user=root");
+        $this->connection = new PDO($dsn);
+    }
+
+    public function query($query){
+
+        $statement = $this->connection->prepare($query);
+        $statement->execute();
+        
+        return $statement;
+    }
+
+}
+
+$db = new Database();
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+
+
+foreach( $posts as $post ){
+    echo "<li>" . $post['title'] . "</li>";
+}
