2d9480e0f4a0cb3e307903106d9c2ae8ea56d64a
Fazle Bari <fazlebarisn@gmail.com>
2022-12-06 16:22:22 +0600

    done

--- note.php
+++ note.php
@@ -6,7 +6,7 @@ $config = require "config.php";
 $heading = 'My Notes';
 $db = new Database($config['database']);
 
-$note = $db->query("select * from notes where id=:id", ['id'=>$_GET['id'] ])->fetch();
+$note = $db->query("select * from notes where id=:id", ['id'=>$_GET['id'] ])->findOrFail();
 
 if( $note ){
     require("views/note.view.php"); 
--- notes.php
+++ notes.php
@@ -7,6 +7,6 @@ $heading = 'My Notes';
 
 $db = new Database($config['database']);
 
-$notes = $db->query("select * from notes where user_id=1")->fetchAll();
+$notes = $db->query("select * from notes where user_id=1")->get();
 
 require("views/notes.view.php"); 
